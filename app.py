6310057264530a30f3464232415c3c2263f8978e
Yashriparmar25 <yashriaparmar@gmail.com>
2026-03-12 18:32:23 +0000

    feat: Final update in UI

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def add_inventory():
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="127.0.0.1", port=5000, debug=True)
-
+ # Change 5000 to any free port, e.g. 5001
+app.run(host="127.0.0.1", port=5001, debug=True)
